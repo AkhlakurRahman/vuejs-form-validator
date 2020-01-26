6f9cf0f24fae8e075470bd009af77017220e9f1d
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-01-26 20:45:35 +0600

    minor fix

--- src/utils/formFields.js
+++ src/utils/formFields.js
@@ -9,7 +9,7 @@ module.exports = {
       type: 'text',
       label: 'First Name',
       placeholder: 'First Name',
-      required: false,
+      required: true,
       validation_message: 'First name is required'
     },
     {
